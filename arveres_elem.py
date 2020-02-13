2378c912e93a0ea283c396697a8786fad62916c6
Ákos Belákovics <akos.belakovics@onbased.com>
2020-02-13 12:41:40 +0100

    Working search

--- arveres_elem.py
+++ arveres_elem.py
@@ -1,3 +1,4 @@
+import argparse
 import requests
 import pprint
 import http.client
@@ -7,7 +8,7 @@ from lxml import etree, html
 pp = pprint.PrettyPrinter(indent=4)
 s = requests.Session()
 
-def main():
+def main(auctionCaseId):
     # conn = http.client.HTTPSConnection("arveres.mbvk.hu")
     # conn.request("POST","/arverezok/reszletezo.php")
     # res = conn.getresponse()
@@ -49,24 +50,41 @@ def main():
     #        '_':'1581525130499'
     #    },
 
-    AUCTION_CASE_ID='0007.V.0608/2012/285'
-    resp = s.get(
-        f'https://arveres.mbvk.hu/arverezok/ajax/server_processing_arver_vendeg_af345689.php?sEcho=1&iColumns=15&sColumns=&iDisplayStart=0&iDisplayLength=10&mDataProp_0=0&mDataProp_1=1&mDataProp_2=2&mDataProp_3=3&mDataProp_4=4&mDataProp_5=5&mDataProp_6=6&mDataProp_7=7&mDataProp_8=8&mDataProp_9=9&mDataProp_10=10&mDataProp_11=11&mDataProp_12=12&mDataProp_13=13&mDataProp_14=14&p_ingatlan=true&p_online=true&p_faziskod=ismetelt_ingatlan&g_sort_mode=&g_sort_dir=&k_vegrehajtasiugyszam={AUCTION_CASE_ID}&k_elo=true&_=1581525130499',
-        headers={
-            'Connection': 'keep-alive',
-            'Pragma': 'no-cache',
-            'Cache-Control': 'no-cache',
-            'Accept': 'application/json, text/javascript, */*; q=0.01',
-            'X-Requested-With': 'XMLHttpRequest',
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36',
-            'Sec-Fetch-Site': 'same-origin',
-            'Sec-Fetch-Mode': 'cors',
-            'Referer': 'https://arveres.mbvk.hu/arverezok/ingatlanhirdetmenyek.php',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Accept-Language': 'hu-HU,hu;q=0.9,en-US;q=0.8,en;q=0.7'
-        }
-    )
-    data = resp.json()
+    TYPES=[
+        'normal_ingatlan',
+        'ismetelt_ingatlan',
+    ]
+    AUCTION_CASE_ID=auctionCaseId
+    data = None
+
+    for propType in TYPES:
+        print(AUCTION_CASE_ID)
+        resp = s.get(
+            f'https://arveres.mbvk.hu/arverezok/ajax/server_processing_arver_vendeg_af345689.php?sEcho=1&iColumns=15&sColumns=&iDisplayStart=0&iDisplayLength=10&mDataProp_0=0&mDataProp_1=1&mDataProp_2=2&mDataProp_3=3&mDataProp_4=4&mDataProp_5=5&mDataProp_6=6&mDataProp_7=7&mDataProp_8=8&mDataProp_9=9&mDataProp_10=10&mDataProp_11=11&mDataProp_12=12&mDataProp_13=13&mDataProp_14=14&p_ingatlan=true&p_online=true&p_faziskod={propType}&g_sort_mode=&g_sort_dir=&k_vegrehajtasiugyszam={AUCTION_CASE_ID}&k_elo=true&_=1581525130499',
+            headers={
+                'Connection': 'keep-alive',
+                'Pragma': 'no-cache',
+                'Cache-Control': 'no-cache',
+                'Accept': 'application/json, text/javascript, */*; q=0.01',
+                'X-Requested-With': 'XMLHttpRequest',
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36',
+                'Sec-Fetch-Site': 'same-origin',
+                'Sec-Fetch-Mode': 'cors',
+                'Referer': 'https://arveres.mbvk.hu/arverezok/ingatlanhirdetmenyek.php',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Accept-Language': 'hu-HU,hu;q=0.9,en-US;q=0.8,en;q=0.7'
+            }
+        )
+        res = resp.json()
+        pp.pprint(res)
+        if res and 'aaData' in res and len(res['aaData']) > 0:
+            data = res
+            break
+    
+    if not data:
+        print('No property found')
+        return
+
     resp = s.post(
         "https://arveres.mbvk.hu/arverezok/reszletezo.php",
         data={ "arveres_id" : data['aaData'][0][0], "arverestetel_id" : data['aaData'][0][1] },
@@ -87,19 +105,32 @@ def main():
             'Accept-Language': 'hu-HU,hu;q=0.9,en-US;q=0.8,en;q=0.7'
         }
     )
-    #pp.pprint(s.cookies)
-    #print(resp.text)
     
     parser = etree.HTMLParser()
     root   = etree.fromstring(resp.text, parser)
-    #elem = root.xpath('//body')
-    # print(html.tostring(elem[0]).decode("iso-8859-2") )
+    
     images = root.xpath('//*[@id="content"]/a/@href')
-    print(images)
-    details =          root.xpath('//body/div[4]/div[2]/div/div[3]/div/div/label')
-    for elem in details:
-        pp.pprint(elem.text)
-    auctionerDetails = root.xpath('//body/div[5]/div[2]/div/div[3]/div/div/label')
-    pp.pprint(auctionerDetails)
 
-main()
+    details=[]
+    detailsNode =          root.xpath('//body/div[4]/div[2]/div/div[3]/div/div/label')
+    for elem in detailsNode:
+        details.append(f'{elem[0].text} {elem[1].text}')
+    
+    auctionerDetails=[]
+    auctionerDetailsNode = root.xpath('//body/div[5]/div[2]/div/div[3]/div/div/label')
+    for elem in auctionerDetailsNode:
+        auctionerDetails.append(f'{elem[0].text} {elem[1].text}')
+    
+    print('Images:')
+    print(images)
+    print('Details:')
+    print('\n'.join(details))
+    print('Auctioner details:')
+    print('\n'.join(auctionerDetails))
+
+
+parser = argparse.ArgumentParser(description='Process command line arguments.')
+parser.add_argument('--auction-case-id', action='store', dest='auctionCaseId', help='The id of the auction case')
+args = parser.parse_args()
+
+main(args.auctionCaseId)
